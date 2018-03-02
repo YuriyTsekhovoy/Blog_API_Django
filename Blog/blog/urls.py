80a1b06b697b8c1d403e8651a8622fc4bd10894b
YuriyTsekhovoy <yuriy.tsekhovoy@gmail.com>
2018-03-02 14:59:19 +0200

    Blog_API_Django

---
+++ Blog/blog/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^posts/$', views.PostList.as_view()),
+]
---
+++ Blog/blog/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
+
+
+class PostList(APIView):
+    def get(self, request, format=None):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
